e8f6d3cb890a12e6ec35322a31c0d79c8add9d25
AugustoM94 <email@github.com>
2023-11-08 16:05:32 +0100

    added components AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div class="jumbotron">
+        <img class="jumbotron-image" src="../../public/img.jpg" alt="Breaking bad img">
+        <h2>Breaking Bad</h2>
+    </div>
+
+
+</template>
+  
+  
+  <script setup>
+  
+  </script>
+  
+  
+  
+  <style lang="css" scoped>
+
+  </style>
